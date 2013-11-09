9b6427d03555cacf98667c54c6677a9afc6a254f
Sumit Savla <susavla@paypal.com>
2013-11-08 18:44:52 -0800

    initial routes

--- server.js
+++ server.js
@@ -15,12 +15,29 @@ var app = express(),
 app.use(express.cookieParser());
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
+app.use(app.router);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
+});
+
+app.get('/', function(req, res) {
+  var gameid = req.cookies.gameid;
+  
+  res.send('gameid: ' + gameid);
+  console.log('gameid: ' + gameid);
+});
+
+app.get('/createGame', function(req, res) {
+  // create game id and store 
+  res.cookie('gameid', req.cookies.gameid+1 || 5000 );
+});
+
+app.get('/join', function(req, res) {
+  res.send('join game');
 });
 
 server.listen(port);
