83b346a4b31420fea0f4d72384b5ad18925044d8
Sam Selvanathan <santhonyselvanat@paypal.com>
2013-11-08 17:45:12 -0800

    added socket.io bare bones

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('1Te9Gvwfrmm8qZKH');
 
-var isProduction = (process.env.NODE_ENV === 'production');
-var http = require('http');
-var port = (isProduction ? 80 : 8000);
+var isProduction = (process.env.NODE_ENV === 'production'),
+    http = require('http'),
+    express = require('express'),
+    io = require('socket.io'),
+    port = (isProduction ? 80 : 8000);
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/synth" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
+var app = express(), 
+    server = http.createServer(app), 
+    io = io.listen(server);
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
+server.listen(port);
 
-  console.log('Server running at http://0.0.0.0:' + port + '/');
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
+
+console.log('Server running at http://0.0.0.0:' + port + '/');
