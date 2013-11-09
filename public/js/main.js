4c48f1a388904ee2c57b928653b582618abb257a
Sumit Savla <susavla@paypal.com>
2013-11-09 15:52:11 -0800

    integration with game api

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,92 @@
 var Application = {
 
 	init: function() {
-		var userData;
+		var userData,
+			gameData,
+		    _this = this;
 		$(document).ready(function() {
 
-			/* var source   = $("#dashboardWrapper").html();
-			console.log(source);
-			var compiled = dust.compileFn(source,"dashboard"); 
-			dust.loadSource(compiled);
-			console.log(compiled); */
-			$.ajax({
-				type: "GET",
-				url: "/user",
-			})
-			.done(function( data ) {
-				dust.render("dashboard", {uname: "sumit"}, function(err, out) {
-					$('#dashboardWrapper').removeClass('hide');
-					$('#dashboardWrapper').append(out);
-					console.log(out);
-				});
-		  	})
-		  	.fail(function( data) {
-		    	$('#register').removeClass('hide');
-		 	});
+			_this.getUserInfo();
+			_this.getGamesInfo();
+
 			$('#registerForm').on('submit', function(evt) {
 				evt.preventDefault();
 				$.ajax({
 					type: "POST",
 					url: "/user",
 					data: $('#registerForm').serialize()
-				})
-				.done(function( data ) {
-			  		dust.render("dashboard", {uname: "Fred"}, function(err, out) {
-						$('#dashboardWrapper').removeClass('hide');
-						console.log(err);
-						console.log(out);
-						$('#dashboardWrapper').append(out);
-					});
-			  	});
-			});
+					})
+					.done(function( data ) {
+				  		$('#register').addClass('hide');
+				  		_this.getUserInfo();
+				  	});
+				});
+
+		});
+	},
+
+	getUserInfo: function() {
+		var _this = this;
+		$.ajax({
+			type: "GET",
+			url: "/user",
+		})
+		.done(function( data ) {
+			userData = data;
+			_this.renderUserInfo(userData);
+	  	})
+	  	.fail(function( data) {
+	    	$('#register').removeClass('hide');
+	 	});
+	},
+
+	getGamesInfo: function() {
+		var _this = this;
+		$.ajax({
+			type: "GET",
+			url: "/games/ready",
+		})
+		.done(function( data ) {
+			gameData = data;
+			_this.renderDashboard(gameData);
+	  	});
+	},
+
+	renderUserInfo: function(userData) {
+		var _this = this;
+		dust.render("userInfo", userData, function(err, out) {
+			$('#userInfo').remove();
+			$('#userInfoWrapper').append(out);
+			$('#dashboard').removeClass('hide');
+			_this.initStartGameBtn();
+		});
+	},
+
+	renderDashboard: function(gameData) {
+		dust.render("gamesInfo", gameData, function(err, out) {
+			$('#gamesInfo').remove();
+			$('#gamesInfoWrapper').append(out);
+			$('#dashboard').removeClass('hide');
+		});
+	},
+
+	initStartGameBtn: function() {
+		var _this = this;
+		$('#startGameBtn').on('click', function(evt) {
+			$.ajax({
+				type: "POST",
+				url: "/game/start"
+			})
+			.done(function( data ) {
+				_this.getGamesInfo();
+		  		alert('Game Started.. waiting for other player...');
+		  	});
 		});
 	}
 
 };
 
 Application.init();
-
-
 
 var socket = io.connect('http://localhost');
 socket.on('news', function (data) {
