502fdfa94d9e43782f4b0688bdec3e239d9af08a
Sam Selvanathan <santhonyselvanat@paypal.com>
2013-11-09 23:44:55 -0800

    added the permission stuff from nko team into the server file

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var mongoClient = require('mongodb').MongoClient,
     nko = require('nko')('1Te9Gvwfrmm8qZKH');
 
 
+// if run as root, downgrade to the owner of this file
+if (process.getuid() === 0) {
+	require('fs').stat(__filename, function(err, stats) {
+		if (err) { return console.error(err); }
+		process.setuid(stats.uid);
+	});
+}    
+
+
 mongoClient.connect('mongodb://127.0.0.1:27017/synth', function(err, db) {
 	if(err) throw err;
 
